6a4eb0deff44f8fe0716cdf3ca8ef882424fcac4
Nizwer Hussain <nizwer@live.com>
2023-09-09 01:35:13 +0530

    BadVPN script enhancements

--- files/badvpn.sh
+++ files/badvpn.sh
@@ -1,27 +1,50 @@
 #!/bin/bash
 
-# Update and upgrade system packages
-sudo apt update && sudo apt upgrade -y
+# Define the installation path
+INSTALL_PATH="/opt/badvpn"
 
-# Install required packages
-sudo apt install -y pkg-config libssl-dev libnspr4-dev libnss3-dev
+# Ensure the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit 1
+fi
 
-# Create and edit the systemd service file
-cat <<EOL | sudo tee /etc/systemd/system/badvpn.service
+# Install dependencies
+apt-get update
+apt-get install -y pkg-config libssl-dev libnspr4-dev libnss3-dev git build-essential cmake
+
+# Clone BadVPN from GitHub
+git clone https://github.com/ambrop72/badvpn.git "$INSTALL_PATH"
+cd "$INSTALL_PATH"
+
+# Build and install BadVPN
+mkdir build
+cd build
+cmake ..
+make
+make install
+
+# Create a symlink to the badvpn-tun2socks binary in /usr/local/bin
+ln -s "$INSTALL_PATH/build/badvpn-tun2socks" /usr/local/bin/badvpn-tun2socks
+
+# Create a systemd service unit file for BadVPN
+cat <<EOF > /etc/systemd/system/badvpn.service
 [Unit]
-Description=BadVPN Service
+Description=BadVPN TUN2SOCKS service
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/badvpn-udpgw --listen-addr 0.0.0.0:7300
+Type=simple
+ExecStart=$INSTALL_PATH/build/badvpn-tun2socks
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
-EOL
+EOF
 
-# Enable and start the BadVPN service
-sudo systemctl enable badvpn.service
-sudo systemctl start badvpn.service
+# Enable the BadVPN service to start at boot and start it
+systemctl enable badvpn.service
+systemctl start badvpn.service
 
-echo "BadVPN service is now enabled and started."
+echo "BadVPN has been installed to $INSTALL_PATH and set up to run at boot."
+echo "You can start it manually using: $INSTALL_PATH/build/badvpn-tun2socks"
